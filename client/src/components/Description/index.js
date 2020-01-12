f27aa9d29adede0b6764820a225ce32c04398d43
Michal <mmatynia@gmail.com>
2020-01-12 14:38:00 +0100

    Languages Done

---
+++ client/src/components/Description/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Desc = (props) => {
+
+    console.log('Description')
+    console.log(props._id)
+
+    // const description = {
+
+    //     img: '/images/featured/featured_home_3.jpg',
+    //     lineOne: 'Up to 40% off',
+    //     lineTwo: 'In second hand guitars',
+    //     linkTitle: 'Shop now',
+    //     linkTo: '/shop',
+
+    // }
+
+    const renderDesc = () => (
+        props.mainText ?
+            <div>
+                {props.mainText}
+            </div>
+            : null
+    )
+
+    return (
+        <div className="home_promotion">
+            {renderDesc()}
+        </div>
+    )
+}
+
+export default Desc
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import HomePromotion from './home_promotion';
-import CardBlock from '../utils/card_block';
+// import HomePromotion from './home_promotion';
+// import CardBlock from '../utils/card_block';
 import BannerBlock from '../utils/Blocks/banner_block';
+import Desc from '../Description';
 
 import { connect } from 'react-redux';
 import { getProductsBySell, getProductsByArrival } from '../../redux/actions/products_actions';
 import { act_getData_Slides } from '../../redux/actions/slides_actions';
+import { act_getDetail_Desc } from '../../redux/actions/desc_actions';
+
 class Home extends Component {
 
     state = {
+        onepage_lg: 'en',
         get_slides:
         {
             sortBy: 'createdAdd',
@@ -18,8 +22,32 @@ class Home extends Component {
         }
     }
 
-    componentDidMount() {
+    componentDidUpdate(prevProps) {
+        // console.log(prevProps)
         // console.log(this.props)
+        if (
+            this.props.user.siteLocalisation !== undefined
+            && this.props.user.siteLocalisation.name !== undefined
+            && prevProps.user.siteLocalisation !== undefined
+            && prevProps.user.siteLocalisation.name !== undefined
+            && this.props.description !== undefined
+            // && this.props.description.descDetail !== undefined
+        ) {
+
+            if (prevProps.user.siteLocalisation.name !== this.props.user.siteLocalisation.name 
+                || this.props.user.siteLocalisation.name !== this.state.onepage_lg
+                ) {
+                // console.log(this.props.user.siteLocalisation.name)
+                this.setState({ onepage_lg: this.props.user.siteLocalisation.name })
+
+                this.props.dispatch(act_getDetail_Desc(this.props.user.siteLocalisation.name))
+            } 
+        }
+
+    }
+
+    componentDidMount() {
+        // console.log(this.props.user.siteLocalisation.name)
 
         this.props.dispatch(getProductsBySell());
         this.props.dispatch(getProductsByArrival());
@@ -28,6 +56,8 @@ class Home extends Component {
         const args = this.state.get_slides
         this.props.dispatch(act_getData_Slides(args));
         // console.log(this.props.products)
+
+        this.props.dispatch(act_getDetail_Desc(this.state.onepage_lg));
     }
 
     render() {
@@ -37,7 +67,9 @@ class Home extends Component {
                     list={this.props.slides.adminGetSlides}
                     title="Banner"
                 />
-                <CardBlock
+                <Desc
+                    {...this.props.description.descDetail} />
+                {/* <CardBlock
                     list={this.props.products.bySell}
                     title="Best selling guitars"
                 />
@@ -45,7 +77,7 @@ class Home extends Component {
                 <CardBlock
                     list={this.props.products.byArrival}
                     title="New Arrivals"
-                />
+                /> */}
             </div>
         )
     }
@@ -55,7 +87,8 @@ const mapStateToProps = (state) => {
 
     return {
         products: state.products,
-        slides: state.slides
+        slides: state.slides,
+        description: state.description,
     }
 }
 
