8c4b602fa6debee4b3c22bc7c8653bb436fc6c1c
Michał Matynia <mmatynia@gmail.com>
2020-05-08 19:10:32 +0200

    Menu Edit in progress 7

--- server/server.js
+++ server/server.js
@@ -1221,6 +1221,9 @@ app.post('/api/menu/update_entity', auth, admin, (req, res) => {
         }
     }
 
+    // console.log(allArgs);
+
+
     Menu.findOneAndUpdate(
         { language: req.query.language, _id: req.query._id },
         {
@@ -1229,44 +1232,74 @@ app.post('/api/menu/update_entity', auth, admin, (req, res) => {
         { new: true },
         (err, doc) => {
             if (!err) {
-            // ========== Recalculate position
-            Menu.
-                find(allArgs)
-                .sort({ position: -1, createdAt: -1 })
-                .exec((err2, doc2) => {
-                    console.log(doc2)
-                    
-                    if (doc2.length > 1) {
+                // ========== Recalculate position
+                Menu.
+                    find({ language: req.query.language })
+                    .sort({ position: 1, createdAt: -1 })
+                    .exec((err2, doc2) => {
+                        // console.log(doc2)
+                        console.log('==========BEGIN============');
 
-                        
+                        if (doc2.length > 1) {
 
-                        let i = 0;
-                        let found = false;
-                        doc2.map(item => {
-                            i = i + 1;
 
-                            if (item._id.toString() === req.query._id.toString() && item.position === req.query.position && found === false) {
 
-                                found = true
+                            let i = 0;
+                            let found = false;
+                            doc2.map(item => {
+                                i = i + 1;
 
-                            } else if (found === true) {
+                                console.log('EachItem')
+                                console.log(item)
+                                console.log(found);
 
-                                Menu.findOneAndUpdate(
-                                    { _id: mongoose.Types.ObjectId(item._id) },
-                                    {
-                                        "$set": {
-                                            position: parseInt(i)
+
+                                if (parseInt(req.body.position) === i && found === false && item._id.toString() !== req.query._id.toString()){
+                                    console.log('INSIDE A1');
+                                    console.log(found);
+
+                                        Menu.findOneAndUpdate(
+                                            { _id: mongoose.Types.ObjectId(item._id) },
+                                            {
+                                                "$set": {
+                                                    position: parseInt(i + 1) 
+                                                }
+                                            }, { new: true },
+                                            (err3, doc3) => {
+                                                console.log(err3);
+                                                
+                                            }
+                                        )
+                                    
+
+                                } else if(item._id.toString() === req.query._id.toString()) {
+                                    console.log('INSIDE A2');
+                                    console.log(found);
+
+                                    found = true
+                                } else if (parseInt(req.body.position) !== i && found === true) {
+                                    console.log('INSIDE A3');
+                                    console.log(found);
+                                    
+                                    console.log(item.linkTo);
+
+
+                                    Menu.findOneAndUpdate(
+                                        { _id: mongoose.Types.ObjectId(item._id) },
+                                        {
+                                            "$set": {
+                                                position: parseInt(i)
+                                            }
+                                        }, { new: true },
+                                        (err3, doc3) => {
                                         }
-                                    }, { new: true },
-                                    (err3, doc3) => {
-                                    }
-                                )
-                            }
-                        })
-                    }
-                })
-            // ==========
-        } 
+                                    )
+                                }
+                            })
+                        }
+                    })
+                // ==========
+            }
 
             if (err) return res.json({ success: false, err });
             return res.status(200).send({ doc })
